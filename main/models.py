251e00d7fc19b641223b7976ac070beb3041bfc6
make_github <abdullayevasilbek1376@gmail.com>
2025-07-23 12:13:09 +0300

    add tasks_list.html

--- main/models.py
+++ main/models.py
@@ -17,4 +17,14 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
+from django.db import models
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=255)
+    doer = models.CharField(max_length=255)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 
