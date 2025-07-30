678934d4aefaefc8a9fdb3030dfc40f41ef6bac5
Muhammadrizochik <mrxmrhacker782@gmail.com>
2025-07-30 13:53:25 +0500

    Fixed BaseModel

--- main/models/base.py
+++ main/models/base.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     added_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='clients')
+
+    class Meta:
+        abstract = True
