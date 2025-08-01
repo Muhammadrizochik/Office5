9a3da07585e9ccbbc719cb52c5210d668b3080af
Muhammadrizochik <mrxmrhacker782@gmail.com>
2025-08-01 15:24:22 +0500

    Fixed

--- main/forms/clients_form.py
+++ main/forms/clients_form.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 from django import forms
 from main.models.clients import Client
 from django.core.exceptions import ValidationError
-=======
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required, user_passes_test
-from main.models.clients import Client
->>>>>>> 65ad6bbf51430822ffa2476fe28b88e4036e5da8
 
 class ClientForm(forms.ModelForm):
     class Meta:
