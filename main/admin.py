d9f7c4231dfd3ef0b3998720e5c7e20474a0996a
ahmadjon07 <ahmadzonzulunov1@email.com>
2025-08-06 13:55:05 +0500

    add admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        return request.user.is_superuser
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+class GroupAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        return request.user.is_superuser
+
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
