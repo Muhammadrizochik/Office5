923342a67ab89e2b117895c807cf5f7252345276
ahmadjon07 <ahmadzonzulunov1@email.com>
2025-08-11 13:29:23 +0500

    add load static

--- main/urls.py
+++ main/urls.py
@@ -1,17 +1,10 @@
-# from django.urls import path
-# from main.views import home_view
-# import main.views as views
 
-from django.urls import path, include
-from django.contrib import admin
-from main.views import views
+
+from django.urls import path
+import main.views as views
 
 urlpatterns = [
-    path('home/', views.home_view, name='home'),
-    path('about/', views.about_view, name='about'),
-    path('contact/', views.contact_view, name='contact'),
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('home/', views.home_view, name='home')
 ]
 
 
