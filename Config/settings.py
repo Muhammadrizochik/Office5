3738e13127b4bbdeda685d233865d49f7c3cafe6
Muhammadyusuf <ahmadaliyevmuhammadyusuf2010@gmail.com>
2025-08-13 14:39:12 +0500

    settings.py, views\user.py

--- Config/settings.py
+++ Config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'Config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'template'],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/views/user.py
+++ main/views/user.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView , LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views.generic.edit import CreateView
 
@@ -8,14 +8,11 @@ class UserCreateView(CreateView):
     form_class = UserCreationForm
     success_url = "/auth/login"
 
+
 class UserLoginView(LoginView):
     template_name = "auth/form.html"
     form_class = AuthenticationForm
 
 
-<<<<<<< HEAD
 class UserLogoutView(LogoutView):
     next_page = "/"
-=======
-
->>>>>>> 9506b04264509a2dba462c66d3108557beaf5c7e
