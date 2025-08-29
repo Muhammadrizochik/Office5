9331f74a5b6490d1f98b201992475c604c018f8a
Muhammadrizochik <mrxmrhacker782@gmail.com>
2025-08-29 14:42:59 +0500

    Fixed Tasks Count

--- main/views/home.py
+++ main/views/home.py
@@ -5,9 +5,9 @@ import main.models as models
 
 def home(request):
     total_tasks = models.Tasks.objects.all().filter(created_by=request.user).count()
-    completed_tasks = models.Tasks.objects.all().filter(status="done", created_by=request.user).count()
-    in_progress_tasks = models.Tasks.objects.all().filter(status="in_progress", created_by=request.user).count()
-    new_tasks = models.Tasks.objects.all().filter(status="new", created_by=request.user).count()
+    completed_tasks = models.Tasks.objects.all().filter(status="tugallangan", created_by=request.user).count()
+    in_progress_tasks = models.Tasks.objects.all().filter(status="jarayyonda", created_by=request.user).count()
+    new_tasks = models.Tasks.objects.all().filter(status="yangi", created_by=request.user).count()
     context = {
         "total_tasks": total_tasks,
         "completed_tasks": completed_tasks,
